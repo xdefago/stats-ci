3b28b5706d113e184fcbbdeb1cbec6ffe3e31bac
xdefago <defago@c.titech.ac.jp>
2023-04-12 18:39:30 +0900

    add comments; rewrite tests

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,19 @@ pub mod quantile;
 
 pub use interval::Interval;
 
-fn z_value(confidence: f64, two_sided: bool) -> f64 {
+///
+/// return the z-value of the normal distribution for a given confidence level.
+/// 
+/// # Arguments
+/// 
+/// * `confidence` - the confidence level, e.g. 0.95 for 95% confidence
+/// * `two_sided` - if true, return the z-value for a two-sided test, otherwise return the z-value for a one-sided test
+/// 
+/// # Panics
+/// 
+/// * if `confidence` is not in the range (0, 1)
+/// 
+pub fn z_value(confidence: f64, two_sided: bool) -> f64 {
     assert!(confidence > 0. && confidence < 1.);
     use statrs::distribution::ContinuousCDF;
     use statrs::distribution::Normal;
@@ -17,14 +29,23 @@ fn z_value(confidence: f64, two_sided: bool) -> f64 {
     n.inverse_cdf(1. - alpha_prime)
 }
 
-fn z_value_one_sided(confidence: f64) -> f64 {
-    z_value(confidence, false)
-}
-
 fn z_value_two_sided(confidence: f64) -> f64 {
     z_value(confidence, true)
 }
 
+///
+/// return the t-value of the t-distribution for a given confidence level.
+/// 
+/// # Arguments
+/// 
+/// * `confidence` - the confidence level, e.g. 0.95 for 95% confidence
+/// * `degrees_of_freedom` - the degrees of freedom of the t-distribution
+/// * `two_sided` - if true, return the t-value for a two-sided test, otherwise return the t-value for a one-sided test
+/// 
+/// # Panics
+/// 
+/// * if `confidence` is not in the range (0, 1)
+/// 
 fn t_value(confidence: f64, degrees_of_freedom: usize, two_sided: bool) -> f64 {
     assert!(confidence > 0. && confidence < 1.);
     use statrs::distribution::ContinuousCDF;
@@ -33,10 +54,6 @@ fn t_value(confidence: f64, degrees_of_freedom: usize, two_sided: bool) -> f64 {
     let t = StudentsT::new(0., 1., degrees_of_freedom as f64).unwrap();
     let alpha_prime = if two_sided { alpha / 2. } else { alpha };
     t.inverse_cdf(1. - alpha_prime)
-}
-
-fn t_value_one_sided(confidence: f64, degrees_of_freedom: usize) -> f64 {
-    t_value(confidence, degrees_of_freedom, false)
 }
 
 fn t_value_two_sided(confidence: f64, degrees_of_freedom: usize) -> f64 {
@@ -51,13 +68,11 @@ mod tests {
     #[test]
     fn test_t_value() {
         for confidence in [0.5, 0.8, 0.9, 0.95, 0.99, 0.999] {
-            let t_value = t_value_two_sided(confidence, 1000);
-            let z_value = z_value_two_sided(confidence);
-            assert_approx_eq!(t_value, z_value, 1e-2);
-
-            let t_value = t_value_one_sided(confidence, 1000);
-            let z_value = z_value_one_sided(confidence);
-            assert_approx_eq!(t_value, z_value, 1e-2);
+            for two_sided in [true, false] {
+                let t_value = t_value(confidence, 1000, two_sided);
+                let z_value = z_value(confidence, two_sided);
+                assert_approx_eq!(t_value, z_value, 1e-2);
+            }
         }
     }
 }
