f74e975faa73401fb9e0df670f7036dfddb42846
Xavier DEFAGO <defago@c.titech.ac.jp>
2023-04-12 10:23:25 +0900

    change to Wilson score intervals

--- src/proportion.rs
+++ src/proportion.rs
@@ -8,12 +8,12 @@
 //!
 //! let data = [
 //!     true, false, true, true, false, true, true, false, true, true,
-//!     false, false, false, true, false, true, false, false, false, false
+//!     false, false, false, true, false, true, false, false, true, false
 //! ];
 //! let confidence = 0.95;
 //! let interval = proportion::ci_true(confidence, data).unwrap();
-//! assert_approx_eq!(interval.low().unwrap(), 0.23, 1e-2);
-//! assert_approx_eq!(interval.high().unwrap(), 0.67, 1e-2);
+//! assert_approx_eq!(interval.low().unwrap(), 0.299, 1e-2);
+//! assert_approx_eq!(interval.high().unwrap(), 0.701, 1e-2);
 //! ```
 //!
 //! # References
@@ -49,9 +49,9 @@ use error::*;
 ///
 /// let data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 /// let confidence = 0.95;
-/// let interval = proportion::ci_if(confidence, &data, |&x| x < 10).unwrap();
-/// assert_approx_eq!(interval.low().unwrap(), 0.23, 1e-2);
-/// assert_approx_eq!(interval.high().unwrap(), 0.67, 1e-2);
+/// let interval = proportion::ci_if(confidence, &data, |&x| x <= 10).unwrap();
+/// assert_approx_eq!(interval.low().unwrap(), 0.299, 1e-2);
+/// assert_approx_eq!(interval.high().unwrap(), 0.701, 1e-2);
 /// ```
 ///
 
@@ -78,12 +78,12 @@ use error::*;
 ///
 /// let data = [
 ///     true, false, true, true, false, true, true, false, true, true,
-///     false, false, false, true, false, true, false, false, false, false
+///     false, false, false, true, false, true, false, false, true, false
 /// ];
 /// let confidence = 0.95;
 /// let interval = proportion::ci_true(confidence, data).unwrap();
-/// assert_approx_eq!(interval.low().unwrap(), 0.23, 1e-2);
-/// assert_approx_eq!(interval.high().unwrap(), 0.67, 1e-2);
+/// assert_approx_eq!(interval.low().unwrap(), 0.299, 1e-2);
+/// assert_approx_eq!(interval.high().unwrap(), 0.701, 1e-2);
 /// ```
 ///
 pub fn ci_true<T: IntoIterator<Item = bool>>(confidence: f64, data: T) -> CIResult<Interval<f64>> {
@@ -122,9 +122,9 @@ pub fn ci_true<T: IntoIterator<Item = bool>>(confidence: f64, data: T) -> CIResu
 ///
 /// let data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 /// let confidence = 0.95;
-/// let interval = proportion::ci_if(confidence, &data, |&x| x < 10).unwrap();
-/// assert_approx_eq!(interval.low().unwrap(), 0.23, 1e-2);
-/// assert_approx_eq!(interval.high().unwrap(), 0.67, 1e-2);
+/// let interval = proportion::ci_if(confidence, &data, |&x| x <= 10).unwrap();
+/// assert_approx_eq!(interval.low().unwrap(), 0.299, 1e-2);
+/// assert_approx_eq!(interval.high().unwrap(), 0.701, 1e-2);
 /// ```
 ///
 pub fn ci_if<T, I: IntoIterator<Item = T>, F: Fn(T) -> bool>(
@@ -166,6 +166,59 @@ pub fn ci_if<T, I: IntoIterator<Item = T>, F: Fn(T) -> bool>(
 /// ```
 ///
 pub fn ci(confidence: f64, population: usize, successes: usize) -> CIResult<Interval<f64>> {
+    ci_wilson(confidence, population, successes)
+}
+
+///
+/// computes the (two sided) confidence interval over the proportion of successes a given sample using the Wilson score interval.
+/// This is the method used by default when calling the function [ci] of this module.
+/// 
+/// # Arguments
+/// 
+/// * `confidence` - the confidence level (must be in (0, 1))
+/// * `population` - the size of the population
+/// * `successes` - the number of successes in the sample
+/// 
+/// # References
+/// 
+/// * [Wikipedia article on Wilson score interval](https://en.wikipedia.org/wiki/Binomial_proportion_confidence_interval#Wilson_score_interval)
+/// * Francis J. DiTraglia. [Blog post: The Wilson Confidence Interval for a Proportion](https://www.econometrics.blog/post/the-wilson-confidence-interval-for-a-proportion/)
+/// 
+pub fn ci_wilson(confidence: f64, population: usize, successes: usize) -> CIResult<Interval<f64>> {
+    if successes > population {
+        return Err(CIError::InvalidSuccesses(successes, population));
+    }
+    if confidence <= 0. || confidence >= 1. {
+        return Err(CIError::InvalidConfidenceLevel(confidence));
+    }
+
+    let n = population as f64;
+    let n_s = successes as f64;
+    let n_f = n - n_s;
+
+    if successes < 2 {
+        // too few successes for statistical significance
+        return Err(CIError::TooFewSuccesses(successes, population, n_s));
+    }
+    if population - successes < 2 {
+        // too few failures for statistical significance
+        return Err(CIError::TooFewFailures(
+            population - successes,
+            population,
+            n_f,
+        ));
+    }
+
+    let z = z_value_two_sided(confidence);
+    let z_2 = z * z;
+
+    let mean = (n_s + z_2 / 2.) / (n + z_2);
+    let span = (z / (n + z_2)) * ( (n_s * n_f / n) + (z_2 / 4.)).sqrt();
+
+    Ok(Interval::new(mean - span, mean + span))
+}
+
+pub fn ci_z_normal(confidence: f64, population: usize, successes: usize) -> CIResult<Interval<f64>> {
     if successes > population {
         return Err(CIError::InvalidSuccesses(successes, population));
     }
@@ -218,7 +271,7 @@ mod tests {
         ];
         let confidence = 0.95;
         let interval = ci_if(confidence, &data, |&x| x <= 10).unwrap();
-        assert_approx_eq!(interval.low().unwrap(), 0.28, 1e-2);
-        assert_approx_eq!(interval.high().unwrap(), 0.72, 1e-2);
+        assert_approx_eq!(interval.low().unwrap(), 0.299, 1e-2);
+        assert_approx_eq!(interval.high().unwrap(), 0.701, 1e-2);
     }
 }
