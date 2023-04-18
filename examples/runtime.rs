4c2b9059e03263fde597f78a3d40df0dced4fd17
Xavier DEFAGO <defago@c.titech.ac.jp>
2023-04-18 12:44:32 +0900

    docs: fix typos in comment of example

--- examples/runtime.rs
+++ examples/runtime.rs
@@ -4,9 +4,9 @@ use std::time::Instant;
 
 ///
 /// Measures the time elapsed to compute a 95% confidence interval for a reasonably large array.
-/// Measurements on a 2022 M2 MacBook Air (M2, 24GB RAM, 1TB SSD) with rustc 1.62.2 yield a runtime of
-/// about 50ms for an array with 12.5M elements (when compiled in --release).
-/// unfirtunately, the runtime seems to hang (or get very slow) when the array size is increased to
+/// Measurements on a 2022 MacBook Air (M2, 24GB RAM, 1TB SSD) with rustc 1.62.2 yield a runtime of
+/// about 50ms for an array of 12.5M elements (when compiled in --release).
+/// Unfortunately, the runtime seems to hang (or get very slow) when the array size is increased to
 /// 15M elements. This could be due to cache issues? I'm not sure.
 /// 
 fn main() {
