bde6ec1d846b3e0d9536b419e0bbcc8dbd623902
Xavier DEFAGO <defago@c.titech.ac.jp>
2023-05-16 01:29:08 +0900

    test: add benchmarks for kahan sum

---
+++ benches/bench_kahan.rs
@@ -0,0 +1,17 @@
+use criterion::{criterion_group, criterion_main, black_box, Criterion};
+use stats_ci::utils::KahanSum;
+
+fn bench_add_assign() {
+    let repetitions = 1_000_000;
+    let mut sum = KahanSum::new(0.0_f32);
+    (1..=repetitions).for_each(|_| {
+        sum += black_box(0.1);
+    });
+}
+
+fn bench_kahan(c: &mut Criterion) {
+    c.bench_function("KahanSum::add_assign", |b| b.iter(|| bench_add_assign()));
+}
+
+criterion_group!(benches, bench_kahan);
+criterion_main!(benches);
